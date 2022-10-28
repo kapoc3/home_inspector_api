746443d85572d931609c11e95233460a58dcddc0
Kapoc <cristiancamilo1033@gmail.com>
2022-10-28 10:36:57 -0500

    implement update method for device

--- HomeInspector/Services/DeviceAppService.cs
+++ HomeInspector/Services/DeviceAppService.cs
@@ -2,6 +2,7 @@
 using HomeInspector.Entities;
 using HomeInspector.Services.Dtos.Device;
 using HomeInspector.Services.Interfaces;
+using Volo.Abp;
 using Volo.Abp.Application.Dtos;
 using Volo.Abp.Application.Services;
 using Volo.Abp.AutoMapper;
@@ -20,7 +21,13 @@ namespace HomeInspector.Services
             _deviceRepository = deviceRepository;
             _mapperAccessor = mapperAccessor;
         }
-        
+
+        /// <summary>
+        /// register a new device into central database
+        /// </summary>
+        /// <param name="input">data for device</param>
+        /// <returns>created device dto</returns>
+        /// <exception cref="UserFriendlyException">when device mac already registered</exception>
         public async Task<DeviceDto> CreateAsync(CreateUpdateDeviceDto input)
         {
             var device = new Device(GuidGenerator.Create());
@@ -30,7 +37,7 @@ namespace HomeInspector.Services
 
             if (exists != null) 
             {
-                throw new Exception("the device with mac {0} already exists");
+                throw new UserFriendlyException(string.Format("the device with mac {0} already exists", input.Mac));
             }
 
             device = await _deviceRepository.InsertAsync(device);
@@ -39,11 +46,40 @@ namespace HomeInspector.Services
             
         }
 
+        /// <summary>
+        /// search a device into database by mac
+        /// </summary>
+        /// <param name="mac">unique value by device</param>
+        /// <returns><device founded instance/returns>
         public async Task<DeviceDto> GetDeviceByMac(string mac)
         {
             var device = await _deviceRepository.FindAsync(d => d.Mac == mac);
             var result = Mapper.Map<Device, DeviceDto>(device);
             return result;
+        }
+
+        /// <summary>
+        /// update device
+        /// </summary>
+        /// <param name="id">unnique identifier</param>
+        /// <param name="input">data to update</param>
+        /// <returns>updated device instance dto</returns>
+        /// <exception cref="UserFriendlyException"></exception>
+        
+        public async Task<DeviceDto> UpdateAsync(Guid id, CreateUpdateDeviceDto input)
+        {
+            var device = await _deviceRepository.FindAsync(d => d.Id == id);
+            if (device == null) 
+            {
+                throw new UserFriendlyException(String.Format("the device id {0} not exists", id));
+            }
+
+            device = Mapper.Map<CreateUpdateDeviceDto, Device>(input, device);
+            await _deviceRepository.UpdateAsync(device);
+
+            var result = Mapper.Map<Device, DeviceDto>(device);
+            return result;
+
         }
 
         public Task DeleteAsync(Guid id)
@@ -57,11 +93,6 @@ namespace HomeInspector.Services
         }
 
         public Task<PagedResultDto<DeviceDto>> GetListAsync(PagedAndSortedResultRequestDto input)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<DeviceDto> UpdateAsync(Guid id, CreateUpdateDeviceDto input)
         {
             throw new NotImplementedException();
         }
